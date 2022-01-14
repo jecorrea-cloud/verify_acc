7bee1c77944e689ce3b027df4d1d10fd272e8e1c
joaquin <jecorrea@protonmail.com>
2022-01-14 13:41:30 -0500

    Program partially working. first item does not fill

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 /* This file is to prevent having two numbers in the same box */
 
 const codes = document.querySelectorAll(".code");
+
+/* Line 7 makes sure the focus stays on the first element when opening the page */
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener("keydown", (e) => {
+    if (e.key >= 0 && e.key <= 9) {
+      codes[idx + 1].focus();
+    }
+  });
+});
