c1b6ecdb57fdf0d9c0e9ac364496a83cc76a6a27
joaquin <jecorrea@protonmail.com>
2022-01-14 13:42:48 -0500

    bug fixed

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ codes[0].focus();
 codes.forEach((code, idx) => {
   code.addEventListener("keydown", (e) => {
     if (e.key >= 0 && e.key <= 9) {
-      codes[idx + 1].focus();
+      setTimeout(() => {
+        codes[idx + 1].focus();
+      }, 10);
     }
   });
 });
